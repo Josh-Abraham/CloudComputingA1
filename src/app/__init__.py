cd3ed17b304d066ad4f709df708ed6a424882355
Josh Abraham <josh.m.n.abraham@gmail.com>
2022-02-01 20:52:32 -0500

    UI_1

---
+++ src/app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+webapp = Flask(__name__)
+
+from app import add_key
+from app import home
+
+
+
+
---
+++ src/app/home.py
@@ -0,0 +1,9 @@
+
+from flask import render_template, url_for
+from app import webapp
+
+
+@webapp.route('/home')
+def home():
+    return render_template("home.html")
+    
