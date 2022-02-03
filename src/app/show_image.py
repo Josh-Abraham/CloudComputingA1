d78cc443d620d86cd8fc374c2b559fd26c1b9ae5
unni12345 <rajkrishnanv@gmail.com>
2022-02-03 05:43:37 -0500

    add show_image functionality

---
+++ src/app/show_image.py
@@ -0,0 +1,15 @@
+from flask import render_template, url_for, request, send_file
+from app import webapp
+
+@webapp.route('/show_image', methods = ['GET','POST'])
+def show_image():
+    if request.method == 'POST':
+        key = request.form['key']
+        return render_template('show_image.html', key=key)
+    return render_template('show_image.html')
+
+# this endpoint just returns the image. The key is the filename with extension
+@webapp.route("/get_image/<key>")
+def get_image(key):
+    filepath = "static/images/" + key
+    return send_file(filepath)
