299ba43dc4466180ac20410ae540b50dbb8c6c8b
vishal kanna <vishalkanna93@gmail.com>
2022-02-10 23:56:18 -0500

    added get and invalidate in memcache

--- src/memcache_app/mem_cache_utils.py
+++ src/memcache_app/mem_cache_utils.py
@@ -4,9 +4,9 @@ import json
 
 @webapp.route('/put', methods = ['POST'])
 def put():
-    req_json = request.get_json(force=True) 
+    req_json = request.get_json(force=True)
     key, value = list(req_json.items())[0]
-    
+
     if key in memcache:
         # Remove old key from dictionary
         memcache.pop(key)
@@ -21,11 +21,37 @@ def clear():
     memcache.clear()
     return get_response()
 
+@webapp.route('/get', methods = ['POST'])
+def get():
+    req_json = request.get_json(force=True)
+    key = list(req_json.items())[0]
+    if key in memcache:
+        return memcache[key]
+    return get_response_no_key()
+
+@webapp.route('/invalidate', methods = ['POST'])
+def invalidate():
+    req_json = request.get_json(force=True)
+    key = list(req_json.items())[0]
+
+    if key in memcache:
+        memcache.pop(key)
+        return get_response()
+    return get_response_no_key()
 
 def get_response():
     response = webapp.response_class(
         response=json.dumps("OK"),
         status=200,
+        mimetype='application/json'
+    )
+
+    return response
+
+def get_response_no_key():
+    response = webapp.response_class(
+        response=json.dumps("Unknown key"),
+        status=400,
         mimetype='application/json'
     )
 
