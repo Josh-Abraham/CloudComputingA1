92dec0806771ac56e4b5ee799c61023a47dd0604
unni12345 <rajkrishnanv@gmail.com>
2022-02-18 11:48:29 -0500

    remove unnecessary change

--- src/app/cache_utils.py
+++ src/app/cache_utils.py
@@ -22,18 +22,6 @@ def set_cache_params(max_capacity, replacement_method):
         query_add = ''' INSERT INTO cache_properties (epoch_date, max_capacity, replacement_method) VALUES (%s,%s,%s)'''
         cursor.execute(query_add,(epoch_date,max_capacity, replacement_method))
         cnx.commit()
-        
-        return epoch_date
-    except:
-        return None
-
-def set_cache_stats(cache_size, key_count, request_count, miss_count):
-    try:
-        cnx = get_db()
-        cursor = cnx.cursor(buffered = True)
-        query_add = ''' INSERT INTO cache_stats (cache_property_id, cache_size, key_count, request_count, miss_count) VALUES (%s,%s,%s, %s,%s)'''
-        cursor.execute(query_add,(cache_size, key_count, request_count, miss_count))
-        cnx.commit()
 
         return epoch_date
     except:
